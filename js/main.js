92f8ff3d0c155c89c9ca440eed16cc8c9126b62a
lllena <lutchencko.lena@gmail.com>
2021-01-04 21:56:19 +0200

    add шото

--- js/main.js
+++ js/main.js
@@ -17,4 +17,10 @@ var hotelSlider = new Swiper('.hotel-slider', {
       nextEl: '.reviews-slider__button--next',
       prevEl: '.reviews-slider__button--prev',
     },
-  })
+  });
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function(){
+    console.log('Клик по кнопке меню')
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
